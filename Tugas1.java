4d23664448fd9d94fec1d5c434a37100de9b8c33
Rici Rahman <rici7rahman@gmail.com>
2021-10-12 13:55:15 +0700

    first commit

---
+++ Tugas1.java
@@ -0,0 +1,41 @@
+public class Tugas1 {
+    public static void main(String[] args){
+        String firstname, lastname,alamat,hobi,makanan ;
+        int age;
+        long phone;
+        double tinggi;
+        char GolonganDarah;
+        Boolean single;
+        
+
+    
+
+        firstname = "Rici ";
+        lastname  = " Rahman";
+        alamat ="Batam";
+        age = 20;
+        phone= +628994847;
+        tinggi = 162.5;
+        GolonganDarah= 'A';
+        single = true;
+        hobi = "Badminton";
+        makanan= "Nasi";
+    
+        
+        
+        System.out.println("Nama     :" + firstname +lastname);
+        System.out.println("alamat   :"+alamat );
+        System.out.println("age      :" +age);
+        System.out.println("phone    :" +phone);
+        System.out.println("Tinggi   :" +tinggi);
+        System.out.println("GolonganDarah :" +GolonganDarah);
+        System.out.println("single   :" +single);
+        System.out.println("hobi   :" +hobi);
+        System.out.println("makanan   :" +makanan);
+
+
+       
+        
+
+    }
+}
